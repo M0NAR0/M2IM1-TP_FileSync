b8d2cf2f55616a100c3fe5042c1d6cf15f0b32da
Morgan <morganlebeau53@gmail.com>
2020-10-26 09:40:54 +0100

    First commit

---
+++ ServiceFileSync.Core/MonService.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.IO;
+
+namespace ServiceFileSync.Core
+{
+    public class MonService
+    {
+        #region Fields
+        /// <summary>
+        ///     Chemin du dossier d'entrée
+        /// </summary>
+        private string _FirstFolderPath;
+
+        /// <summary>
+        ///     Chemin du dossier de sortie
+        /// </summary>
+        private string _SecondFolderPath;
+
+        /// <summary>
+        ///     Classe d'écoute du premier répertoire
+        /// </summary>
+        private FileSystemWatcher _FirstWatcher;
+
+        /// <summary>
+        ///     Classe d'écoute du second répertoire
+        /// </summary>
+        private FileSystemWatcher _SecondWatcher;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        ///     Initialise une nouvelle instance de la classe <see cref="MonService"/>.
+        /// </summary>
+        /// <param name="firstFolderPath">Chemin du premier répertoire.</param>
+        /// <param name="secondFolderPath">Chemin du second répertoire.</param>
+        public MonService(string firstFolderPath, string secondFolderPath)
+        {
+            this._FirstFolderPath = !string.IsNullOrWhiteSpace(firstFolderPath) ?
+                                        firstFolderPath : throw new ArgumentNullException(nameof(firstFolderPath));
+            this._SecondFolderPath = !string.IsNullOrWhiteSpace(secondFolderPath) ?
+                                        secondFolderPath : throw new ArgumentNullException(nameof(secondFolderPath));
+        }
+        #endregion
+
+        #region Methods
+
+        #region Service
+
+        /// <summary>
+        ///     Démarre le service.
+        /// </summary>
+        public void Start()
+        {
+            if (this._FirstWatcher == null)
+            {
+                try
+                {
+                    //Créer le premier dossier s'il n'existe pas.
+                    Directory.CreateDirectory(this._FirstFolderPath);
+                    //Créer le second dossier s'il n'existe pas.
+                    Directory.CreateDirectory(this._SecondFolderPath);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Impossible de créer le dossier d'entrée ou de sortie", ex);
+                }
+
+                this._FirstWatcher = new FileSystemWatcher(this._FirstFolderPath);
+                this._FirstWatcher.Created += this.OneWay;
+                this._FirstWatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        /// <summary>
+        ///     Met en pause l'exécution du service.
+        /// </summary>
+        public void Pause()
+        {
+            if (this._FirstWatcher != null && this._FirstWatcher.EnableRaisingEvents)
+            {
+                this._FirstWatcher.EnableRaisingEvents = false;
+            }
+        }
+
+        /// <summary>
+        ///     Reprend l'exécution du service.
+        /// </summary>
+        public void Continue()
+        {
+            if (this._FirstWatcher != null && !this._FirstWatcher.EnableRaisingEvents)
+            {
+                this._FirstWatcher.EnableRaisingEvents = true;
+            }
+        }
+
+        /// <summary>
+        ///     Arrête l'exécution du service.
+        /// </summary>
+        public void Stop()
+        {
+            if (this._FirstWatcher != null)
+            {
+                //this._FirstWatcher.Created -= this.Watcher_Created;
+                this._FirstWatcher.Dispose();
+                this._FirstWatcher = null;
+            }
+        }
+
+        #endregion
+
+        private void OneWay(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                //Clear second folder
+                if (Directory.Exists(this._SecondFolderPath))
+                {
+                    //Remove the existing files
+                    string[] files = Directory.GetFiles(this._SecondFolderPath);
+
+                    foreach (string file in files)
+                    {
+                        File.Delete(file);
+                    }
+
+                    //Remove the existing directories
+                    string[] directories = Directory.GetDirectories(this._SecondFolderPath);
+
+                    foreach (string directory in directories)
+                    {
+                        Directory.Delete(directory);
+                    }
+                }
+
+                //Copy first folder to the second
+                if (Directory.Exists(this._FirstFolderPath))
+                {
+                    //Copy the files
+                    string[] files = Directory.GetFiles(this._FirstFolderPath);
+
+                    foreach (string s in files)
+                    {
+                        string fileName = Path.GetFileName(s);
+                        string destFile = Path.Combine(this._SecondFolderPath, fileName);
+                        File.Copy(s, destFile, true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Impossible d'effectuer la synchronisation OneWay", ex);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ServiceFileSync/Service1.cs
@@ -0,0 +1,54 @@
+﻿using ServiceFileSync.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFileSync
+{
+    public partial class Service1 : ServiceBase
+    {
+        #region Fields
+
+        /// <summary>
+        ///     Instance du service.
+        /// </summary>
+        private readonly MonService _Service;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initialise une nouvelle instance de la classe <see cref="Service1"/>.
+        /// </summary>
+        public Service1()
+        {
+            InitializeComponent();
+
+            // TOCHANGE
+            this._Service = new MonService(@"C:\TMP\INPUT", @"C:\TMP\OUTPUT");
+            //Permet d'accepter la mise en pause et la reprise du service.
+            this.CanPauseAndContinue = true;
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStart(string[] args) => this._Service.Start();
+
+        protected override void OnStop() => this._Service.Stop();
+
+        protected override void OnPause() => this._Service.Pause();
+
+        protected override void OnContinue() => this._Service.Continue();
+
+        #endregion
+    }
+}
